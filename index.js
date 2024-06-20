bad7c8a348b226c2aaf40078dae2b8dcc1408ba8
Isabela_ <isabela.rosero@correounivalle.edu.co>
2024-06-20 09:39:56 -0500

    new range

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ router.post('/', async ( req, res) => {
           range = 'PROGRAMAS!A1:AH93';
           break;
         case 'Seguimientos':
-          range = 'SEGUIMIENTOS!A1:G1000';
+          range = 'SEGUIMIENTOS!A1:H1000';
           break;
         case 'Permisos':
           range = 'PERMISOS!A1:C20';
